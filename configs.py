cba37536abfb32dd39c548e7237b5055de0a1968
jackgrimes <jack.e.t.grimes@gmail.com>
2019-11-15 09:07:44 +0000

    further tidying

--- configs.py
+++ configs.py
@@ -1,2 +1,10 @@
 CSV_PATH = r"C:\dev\data\puregym_n_people\gym_people_counts.csv"
 CREDENTIALS_PATH = "C:\dev\data\puregym_n_people\puregym_credentials.json"
+
+LOGIN_URL = 'https://www.puregym.com/login/'
+LOGIN_API_URL = 'https://www.puregym.com/api/members/login'
+MEMBERS_URL = 'https://www.puregym.com/members/'
+
+# In seconds
+TIME_BETWEEN_SCRAPES = 270
+TIME_BETWEEN_RETRIES = 1
